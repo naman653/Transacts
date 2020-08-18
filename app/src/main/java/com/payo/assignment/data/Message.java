47585c36ac64371c4964dd110a2d53bcdd605c5d
naman653 <naman653@gmail.com>
2020-08-18 07:58:54 +0530

    update Message.java

--- app/src/main/java/com/payo/assignment/data/Message.java
+++ app/src/main/java/com/payo/assignment/data/Message.java
@@ -1,5 +1,46 @@
 package com.payo.assignment.data;
 
-class Message {
+public class Message {
+    private String senderId;
+    private String body;
+    private double amount;
+    private TransactionType type;
 
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public enum TransactionType {
+        DEBIT,
+        CREDIT,
+        UNDEFINED
+    }
 }
